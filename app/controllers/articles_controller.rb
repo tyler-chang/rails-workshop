7df10cfb5a87b006d304ed752c878eecde09cc7b
XieSong <563527588@qq.com>
2019-01-09 10:53:45 +0800

    "add article destroy"

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -34,6 +34,12 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
   private
   def article_params
     params.require(:article).permit(:title, :text)
