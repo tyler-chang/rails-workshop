facb9b57e89cf3d50365a750eaf0d5cb164f0103
XieSong <563527588@qq.com>
2019-01-09 09:49:16 +0800

    "add article edit"

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,6 +6,10 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(article_params)
 
@@ -13,6 +17,16 @@ class ArticlesController < ApplicationController
       redirect_to @article
     else
       render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
     end
   end
 
