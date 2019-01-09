535450ed60b13c71a0fa7db4155b5bd7151a2068
Tyler Chang <TylerChangMore@gmail.com>
2019-01-09 17:17:48 +0800

    workshop day2

---
+++ app/models/article.rb
@@ -0,0 +1,15 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  text       :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Article < ApplicationRecord
+  # validates :title, presence: true,
+  #                   length: { minimum: 5 }
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,25 @@
+# == Route Map
+#
+#                    Prefix Verb   URI Pattern                                                                              Controller#Action
+#             welcome_index GET    /welcome/index(.:format)                                                                 welcome#index
+#                  articles GET    /articles(.:format)                                                                      articles#index
+#                           POST   /articles(.:format)                                                                      articles#create
+#               new_article GET    /articles/new(.:format)                                                                  articles#new
+#              edit_article GET    /articles/:id/edit(.:format)                                                             articles#edit
+#                   article GET    /articles/:id(.:format)                                                                  articles#show
+#                           PATCH  /articles/:id(.:format)                                                                  articles#update
+#                           PUT    /articles/:id(.:format)                                                                  articles#update
+#                           DELETE /articles/:id(.:format)                                                                  articles#destroy
+#                      root GET    /                                                                                        welcome#index
+#        rails_service_blob GET    /rails/active_storage/blobs/:signed_id/*filename(.:format)                               active_storage/blobs#show
+# rails_blob_representation GET    /rails/active_storage/representations/:signed_blob_id/:variation_key/*filename(.:format) active_storage/representations#show
+#        rails_disk_service GET    /rails/active_storage/disk/:encoded_key/*filename(.:format)                              active_storage/disk#show
+# update_rails_disk_service PUT    /rails/active_storage/disk/:encoded_token(.:format)                                      active_storage/disk#update
+#      rails_direct_uploads POST   /rails/active_storage/direct_uploads(.:format)                                           active_storage/direct_uploads#create
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'welcome/index'
+  resources :articles
+  root 'welcome#index'
 end
